1f4b9eaa537f030fe672c2ec0ef7956956ad69cb
Han <hjing@dhcp-nyc-174.redrover.cornell.edu>
2017-10-13 08:40:44 -0400

    working export buttons

--- js/datatable.js
+++ js/datatable.js
@@ -1,53 +1,58 @@
 function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
+  return text;
 }
 
 var adapter = new LocalStorage('db');
 // const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-// Set some defaults
-db.defaults({
-        posts: [],
-        user: {}
-    })
-    .write()
+// // Set some defaults
+// db.defaults({ posts: [], user: {} })
+//   .write()
+//
+// // Add a post
+// db.get('posts')
+//   .push({ id: 1, title: 'lowdb is awesome'})
+//   .write()
+//
+// // Set a user using Lodash shorthand syntax
+// db.set('user.name', 'typicode')
+//   .write()
+//
+// console.log(makeid());
+//
+// db.write();
+// console.log('State has been saved')
+// var foo = db.read('posts');
+// console.log(foo);
+// console.log('State has been updated')
+// db.set('ID', makeid()).write();
+var dataset = [];
+$(document).ready(function(){
 
-// Add a post
-db.get('posts')
-    .push({
-        id: 1,
-        title: 'lowdb is awesome'
-    })
-    .write()
-
-// Set a user using Lodash shorthand syntax
-db.set('user.name', 'typicode')
-    .write()
-
-console.log(makeid());
-
-db.write();
-console.log('State has been saved')
-var foo = db.read('posts');
-console.log(foo);
-console.log('State has been updated')
-db.set('ID', makeid()).write();
-
-$(document).ready(function() {
-    $('#datatable').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        iDisplayLength: Math.round(($("body").height() * .55) / $("#datatable tr").height())
-    });
-    $('#datatable_filter').addClass('pull-left');
-    $('#datatable_wrapper').prepend("<button class='btn btn-info btn-sm pull-right'>Export CSV</button>");
+  $('#datatable').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        'copyHtml5',
+        'excelHtml5',
+        'csvHtml5',
+        'pdfHtml5'
+    ],
+    // data: dataset,
+    // columns: [
+    //  { title: "ID" },
+    //  { title: "Street" },
+    //  { title: "Condition" },
+    //  { title: "Type" },
+    //  { title: "Text" },
+    //  { title: "Image URL" },
+    //  { title: "Last Updated" },
+    // ]
+  });
 });
