4cef9c729183e10c5890bb38f0971bec15f541a5
Han <hjing@dhcp-nyc-174.redrover.cornell.edu>
2017-10-12 22:23:53 -0400

    add datatable with basic db object constructs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 to start the server, run
 
 ./setup.sh
+
+
+To enable local storage of data you have change the privacy content in your chrome browser
+
+https://www.chromium.org/for-testers/bug-reporting-guidelines/uncaught-securityerror-failed-to-read-the-localstorage-property-from-window-access-is-denied-for-this-document
--- js/datatable.js
+++ js/datatable.js
@@ -1,5 +1,45 @@
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+var adapter = new LocalStorage('db');
+// const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// Set some defaults
+db.defaults({ posts: [], user: {} })
+  .write()
+
+// Add a post
+db.get('posts')
+  .push({ id: 1, title: 'lowdb is awesome'})
+  .write()
+
+// Set a user using Lodash shorthand syntax
+db.set('user.name', 'typicode')
+  .write()
+
+console.log(makeid());
+
+db.write();
+console.log('State has been saved')
+var foo = db.read('posts');
+console.log(foo);
+console.log('State has been updated')
+db.set('ID', makeid()).write();
 
 $(document).ready(function(){
-  $('#datatable').DataTable();
+
+  $('#datatable').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
 });
